705a2ed07bc12f20dacc0d6caffcfc91b984f1ba
Anuhya Paturu <paturuanuhya@gmail.com>
2026-02-01 22:56:31 -0800

    link frontend to live koyeb backend

--- client/src/App.js
+++ client/src/App.js
@@ -81,7 +81,7 @@ function App() {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/chat', {
+      const response = await fetch('https://sorry-fayth-self-employee-907f0163.koyeb.app/api/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message: query }),
